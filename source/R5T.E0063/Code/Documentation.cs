140c266483b50ea8c695c4191b5c1597a7431ace
davidcoats <david@getsafetycone.com>
2022-11-16 11:08:13 -0800

    Initial commit.

---
+++ source/R5T.E0063/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.E0063
+{
+	/// <summary>
+	/// Render a Razor component to string.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.E0063/Code/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace R5T.E0063
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
